49c3b2cb798360ff222df5972cf84d0de7577483
Tung Teeranut <tpraisuwan@gmail.com>
2021-06-17 14:45:36 +0700

    ggoplt labs ans annotate

--- ggplot title.R
+++ ggplot title.R
@@ -17,3 +17,23 @@ ggplot(data = penguins)+
        caption = "Penguins dataset")+
   annotate("text", x=220, y=3500, label= "The Gentoos are the largest", 
            color = "grey", fontface = "bold", size = 4.5, angle =30)
+
+#p is geom_point
+p <- ggplot(mtcars, aes(x = wt, y = mpg)) + geom_point()
+
+p + annotate("rect", xmin = 3, xmax = 4.2, ymin = 12, ymax = 21,
+          alpha = .2)
+
+p + annotate("pointrange", x = 3.5, y = 20, ymin = 12, ymax = 28,
+             colour = "red", size = 1.5)
+
+p + annotate("text", x = 4, y = 25, label = "italic(R) ^ 2 == 0.75",
+             parse = TRUE)
+
+p + annotate("text", x = 4, y = 25, label = "Some text")
+
+p + annotate("text", x = 2:5, y = 25, label = "Some text")
+
+
+
+
