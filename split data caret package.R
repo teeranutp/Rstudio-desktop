ed57e658ea15257b75cbb25de8a23f5cc74d91ea
Tung Teeranut <tpraisuwan@gmail.com>
2021-06-18 23:22:34 +0700

    split data caret package

---
+++ split data caret package.R
@@ -0,0 +1,22 @@
+install.packages("caret")
+library(caret)
+library(tidyverse)
+library(mlbench)
+
+data(Sonar)
+tibble(Sonar)
+
+#split data
+set.seed(99)
+train_id <- createDataPartition(
+  Sonar$Class,
+  p = 0.8,
+  list = FALSE
+)
+
+
+train_data <- Sonar[train_id, ]
+test_data <- Sonar[-train_id, ]
+
+nrow(train_data)
+nrow(test_data)
